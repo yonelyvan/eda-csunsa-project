83f7dc454f3017cfd2dc2b5017a6dffcd41583ad
Yonel Yvan Mamani <yonelyvan@gmail.com>
2017-01-04 09:44:14 -0500

    Create comandos_postgres.sql

---
+++ comandos_postgres.sql
@@ -0,0 +1,19 @@
+-----------config
+
+export PATH=$HOME/project/bin:$PATH
+export PGDATA=DemoDir
+initdb
+
+----------comandos
+psql -l       --ver/listar bases de datos
+psql name_db  --seleccionar una base de datos "name_db"
+\dt           --ver/listar tablas
+\df           --ver/listar funciones
+
+\q            --salir
+
+
+----------- algunas consultas
+
+
+
